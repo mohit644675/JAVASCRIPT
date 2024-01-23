651f7245247dd2ed6e648dfcb22293cf9514232a
mohit644675 <137153766+mohit644675@users.noreply.github.com>
2024-01-23 07:43:47 +0000

    02-variables

---
+++ 01-basics/02-variables.js
@@ -0,0 +1,17 @@
+const accountId = "144553"
+let accountEmail="mk644675@gmail.com"
+var accountPassword="12345"
+accountCity="Patna"
+
+//accountId = 2
+accountEmail = "mk655657@gmail.com"
+accountPassword = "123321"
+accountCity = "Saran"
+let accounState;
+
+/*
+  only use let (issue in var Scope)
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accounState])
+
