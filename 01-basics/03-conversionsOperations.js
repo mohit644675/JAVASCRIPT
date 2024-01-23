d8675efe5283d0bbbd2b45e2c58be3c48c00ea9b
mohit644675 <137153766+mohit644675@users.noreply.github.com>
2024-01-23 11:00:33 +0000

    Conversion of data types

---
+++ 01-basics/03-conversionsOperations.js
@@ -0,0 +1,16 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
